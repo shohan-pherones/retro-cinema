60c4b2b8a41aff7385b0256fb7acf5ed1cf79e2a
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-05 21:00:37 +0600

    total added

--- script.js
+++ script.js
@@ -146,11 +146,10 @@ const moviesArr = [
   movie16,
 ];
 
-// Elements
-const moviesEl = document.querySelector(".movies");
-
 // Display movies
 function displayMovies() {
+  const moviesEl = document.querySelector(".movies");
+
   for (let i = 0; i < moviesArr.length; i++) {
     const movie = document.createElement("div");
     movie.classList.add(
@@ -162,6 +161,7 @@ function displayMovies() {
       "border-0",
       "rounded-3"
     );
+
     movie.innerHTML = `
         <div class="movie-poster">
             <img
@@ -171,16 +171,62 @@ function displayMovies() {
         </div>
         <div class="card-body movie-details">
             <h5 class="card-title fw-bold">
-              ${moviesArr[i].title} (<span class="year">${moviesArr[i].year}</span>)
+              ${moviesArr[i].title} (${moviesArr[i].year})
             </h5>
             <h5 class="fw-bold text-danger">
               BDT. <span class="ticket-price">${moviesArr[i].ticketPrice}</span>
             </h5>
-            <a href="#" class="btn btn-dark fw-bold book-ticket">Book Ticket</a>
+            <button class="btn btn-dark fw-bold book-ticket">Book Ticket</button>
         </div>
   `;
     moviesEl.append(movie);
   }
 }
-
 displayMovies();
+
+// Display total
+function displayTotal() {
+  const bookTickets = document.querySelectorAll(".book-ticket");
+  const modalWindow = document.querySelector(".modal-total");
+
+  for (let i = 0; i < bookTickets.length; i++) {
+    bookTickets[i].addEventListener("click", function () {
+      for (let j = 0; j < moviesArr.length; j++) {
+        if (i === j) {
+          const title = moviesArr[j].title;
+          const year = moviesArr[j].year;
+          const price = moviesArr[j].ticketPrice;
+          const VAT = (price * 10) / 100;
+          const subTotal = price + VAT;
+
+          modalWindow.style.opacity = 1;
+          modalWindow.style.visibility = "visible";
+
+          modalWindow.innerHTML = `
+          <span class="cross">❌</span>
+          <h2 class="fw-bold">Movie Title: ${title} (${year})</h2>
+          <h5>Price: BDT. ${price}</h5>
+          <h5>VAT: BDT. ${VAT}</h5>
+          <h4 class="text-danger fw-bold mt-3">Subtotal: BDT. ${subTotal}</h4>
+          <button class="btn btn-info fw-bold mt-3 shadow buy-now">Buy Now</button>
+          `;
+
+          // Hide modal
+          const crossModal = document.querySelector(".cross");
+          const buyNow = document.querySelector(".buy-now");
+
+          crossModal.addEventListener("click", function () {
+            modalWindow.style.opacity = 0;
+            modalWindow.style.visibility = "hidden";
+          });
+
+          buyNow.addEventListener("click", function () {
+            modalWindow.style.opacity = 0;
+            modalWindow.style.visibility = "hidden";
+          });
+        }
+      }
+    });
+  }
+}
+displayTotal();
