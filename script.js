5bd4be97b28fb5c8aed20242f325a482d13975cc
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-06 17:37:23 +0600

    movies setted

--- script.js
+++ script.js
@@ -1,275 +1,12 @@
-//  Movies
-const movie1 = {
-  title: "Top Gun: Maverick",
-  year: 2022,
-  ticketPrice: 375,
-  posterURL:
-    "https://www.torrentbd.com/posters/P7J6QBtsk758rCVZ0TajtQYY1745960.jpg",
-};
-
-const movie2 = {
-  title: "Orphan: First Kill",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/CQ7uuODTRQD74EEfJPDwUQYY11851548.jpg",
-};
-
-const movie3 = {
-  title: "Nope",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/2BWAUqwpFb62mcHiEntXaAYY10954984.jpg",
-};
-
-const movie4 = {
-  title: "The Next 365 Days",
-  year: 2022,
-  ticketPrice: 375,
-  posterURL:
-    "https://www.torrentbd.com/posters/grT28qkCjf9qbW421hzHFAYY21106646.jpg",
-};
-
-const movie5 = {
-  title: "Fall",
-  year: 2022,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/Enz19xyc88dsEAMYF5jFwgYY15325794.jpg",
-};
-
-const movie6 = {
-  title: "House of the Dragon",
-  year: 2022,
-  ticketPrice: 380,
-  posterURL:
-    "https://www.torrentbd.com/posters/oUw3f9JJagihLRybyY7P5wYY11198330.jpg",
-};
-
-const movie7 = {
-  title: "The Sandman",
-  year: 2022,
-  ticketPrice: 380,
-  posterURL:
-    "https://www.torrentbd.com/posters/BD8VXOgCq4d4P2vBeiG3qAYY1751634.jpg",
-};
-
-const movie8 = {
-  title: "Purple Hearts",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/1tOHYiUmdR4eeA1KEw8XdwYY4614584.jpg",
-};
-
-const movie9 = {
-  title: "The Princess",
-  year: 2022,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/ARNOJg3bwASSfJ7cCveoKAYY13406136.jpg",
-};
-
-const movie10 = {
-  title: "Into the Deep",
-  year: 2022,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/zZnz5m2SkIqmwjuRjgwMCAYY11005128.jpg",
-};
-
-const movie11 = {
-  title: "Little Ukraine",
-  year: 2022,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/2QuXs4NbZNZ5XhjOJlyv2QYY11511518.jpg",
-};
-
-const movie12 = {
-  title: "Prey",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/DegyyZ5ZNP7LoOiVvnoN1AYY11866324.jpg",
-};
-
-const movie13 = {
-  title: "The Moderator",
-  year: 2022,
-  ticketPrice: 330,
-  posterURL:
-    "https://www.torrentbd.com/posters/e2xU0RqLZ4LVuoD3Nj4XHwYY12810902.jpg",
-};
-
-const movie14 = {
-  title: "Irul",
-  year: 2021,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/p4a9JExC9FnZuxqHZ7326wYY13103212.jpg",
-};
-
-const movie15 = {
-  title: "Vikram",
-  year: 2022,
-  ticketPrice: 380,
-  posterURL:
-    "https://www.torrentbd.com/posters/idJqYus6zjdGAoERtKkLAAYY9179430.jpg",
-};
-
-const movie16 = {
-  title: "Thor: Ragnarok",
-  year: 2017,
-  ticketPrice: 375,
-  posterURL:
-    "https://www.torrentbd.com/posters/qhuAOl4wo1zZWqhQYSKb4wYY3501632.jpg",
-};
-
-const movie17 = {
-  title: "John Wick: Chapter 3",
-  year: 2019,
-  ticketPrice: 375,
-  posterURL:
-    "https://www.torrentbd.com/posters/LdJpOpljc1qQLd8fc0dxfAYY6146586.jpg",
-};
-
-const movie18 = {
-  title: "Crawdads Sing",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/KK1tKGTKPWrBBViiSbrPtgYY9411972.jpg",
-};
-
-const movie19 = {
-  title: "Burial",
-  year: 2022,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/YMeJB8Ah2uUKRZhG9CTy9QYY13063384.jpg",
-};
-
-const movie20 = {
-  title: "Nobody",
-  year: 2021,
-  ticketPrice: 380,
-  posterURL:
-    "https://www.torrentbd.com/posters/WHuJmhQieBauN1MT3VopMQYY7888964.jpg",
-};
-
-const movie21 = {
-  title: "The Perfect Nanny",
-  year: 2001,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/XXqi6ZEjN0gooMZ3l3fDGgYY0228711.jpg",
-};
-
-const movie22 = {
-  title: "Mad Max: Fury Road",
-  year: 2015,
-  ticketPrice: 340,
-  posterURL:
-    "https://www.torrentbd.com/posters/DEm1RNqcmcBUlSRD86zEhwYY1392190.jpg",
-};
-
-const movie23 = {
-  title: "Sinister 2",
-  year: 2015,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/eNNc9qZiXY5c1emd9DkD1QYY2752772.jpg",
-};
-
-const movie24 = {
-  title: "[REC] 3: Genesis",
-  year: 2012,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/ICpuyH9T9Bn9NPH2SwHGYgYY1649444.jpg",
-};
-
-const movie25 = {
-  title: "Seoul Vibe",
-  year: 2022,
-  ticketPrice: 340,
-  posterURL:
-    "https://www.torrentbd.com/posters/c8MAKiGa3IpeelqAuKI2xgYY13611770.jpg",
-};
-
-const movie26 = {
-  title: "Oculus",
-  year: 2013,
-  ticketPrice: 340,
-  posterURL:
-    "https://www.torrentbd.com/posters/wfJCmxaJwDe6dKPNdoQuRgYY2388715.jpg",
-};
-
-const movie27 = {
-  title: "The Northman",
-  year: 2022,
-  ticketPrice: 370,
-  posterURL:
-    "https://www.torrentbd.com/posters/v8mUR8Hrr2d2BzrdBNQe0AYY11138512.jpg",
-};
-
-const movie28 = {
-  title: "Spider's Web",
-  year: 2018,
-  ticketPrice: 320,
-  posterURL:
-    "https://www.torrentbd.com/posters/xxZx0jZ8WDzETApwCWFnZAYY5177088.jpg",
-};
-
-const movie29 = {
-  title: "Greyhound",
-  year: 2020,
-  ticketPrice: 350,
-  posterURL:
-    "https://www.torrentbd.com/posters/47zQxEZwoDzvGYZ0Sxzn9gYY6048922.jpg",
-};
-
-const movie30 = {
-  title: "Batman & Robin",
-  year: 1997,
-  ticketPrice: 330,
-  posterURL:
-    "https://www.torrentbd.com/posters/UXxTzdnTlbI5JZWZSLsR9AYY0118688.jpg",
-};
-
+// Movies
 const moviesArr = [
-  movie1,
-  movie2,
-  movie3,
-  movie4,
-  movie5,
-  movie6,
-  movie7,
-  movie8,
-  movie9,
-  movie10,
-  movie11,
-  movie12,
-  movie13,
-  movie14,
-  movie15,
-  movie16,
-  movie17,
-  movie18,
-  movie19,
-  movie20,
-  movie21,
-  movie22,
-  movie23,
-  movie24,
-  movie25,
-  movie26,
-  movie27,
-  movie28,
-  movie29,
-  movie30,
+  {
+    title: "Tiny Cinema",
+    year: 2022,
+    ticketPrice: 450,
+    posterURL:
+      "https://www.torrentbd.com/posters/Gx6C93kIiKdvtUWK6tP7hAYY15908746.jpg",
+  },
 ];
 
 // Display movies
@@ -299,12 +36,12 @@ function displayMovies() {
             <h5 class="card-title fw-bold">
               ${moviesArr[i].title} (${moviesArr[i].year})
             </h5>
-            <h5 class="fw-bold text-danger">
-              BDT. <span class="ticket-price">${moviesArr[i].ticketPrice}</span>
+            <h5 class="fw-bold text-danger"><span class="ticket-price">${moviesArr[i].ticketPrice}৳</span>
             </h5>
             <button class="btn btn-dark fw-bold book-ticket">Book Ticket</button>
         </div>
   `;
+
     moviesEl.append(movie);
   }
 }
@@ -331,10 +68,10 @@ function displayTotal() {
           modalWindow.innerHTML = `
           <span class="cross">❌</span>
           <h2 class="fw-bold">${title} (${year})</h2>
-          <h5><strong>Price:</strong> BDT. ${price}</h5>
-          <h5><strong>VAT:</strong> BDT. ${VAT}</h5>
+          <h5><strong>Price: </strong>${price}৳</h5>
+          <h5><strong>VAT: </strong>${VAT}৳</h5>
           <hr>
-          <h4 class="text-danger fw-bold mt-3">Subtotal: BDT. ${subTotal}</h4>
+          <h4 class="text-danger fw-bold mt-3">Subtotal: ${subTotal}৳</h4>
           <button class="btn btn-danger fw-bold mt-3 buy-now">Buy Now</button>
           `;
 
